099a4577fcd916983e78b1f0c4d6c60aa29265e7
Bruno Santos <>
2022-12-07 18:46:15 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Container from './components/Container';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>Vite</Container>
+    </div>
+  );
+}
+
+export default App;
