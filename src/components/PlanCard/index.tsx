79d6c6c83c588ec8ace2a7aa35730201d3161811
Bruno Santos <>
2022-12-11 20:43:27 +0000

    refactor: plan card selection dispatch

--- src/components/PlanCard/index.tsx
+++ src/components/PlanCard/index.tsx
@@ -1,33 +1,53 @@
 /* eslint-disable react/require-default-props */
 import React from 'react';
+import { PlanOptions } from '~/constants/planCards';
+import PriceModel from '~/constants/priceModel';
+import { useAppDispatch, useAppSelector } from '~/hooks';
+import { selectPlan } from '~/store/planStepSlice';
 import styles from './styles.module.scss';
 
 interface PlanCardProps {
   element: {
-    title: string;
+    title: PlanOptions;
     price: {
       monthly: string;
       yearly: string;
     };
     icon: string;
   };
-  onClick: React.Dispatch<React.SetStateAction<string | null>>;
-  selected?: boolean;
-  yearly?: boolean;
 }
 
-const PlanCard = ({ element, onClick, selected, yearly }: PlanCardProps) => (
-  <li
-    className={`${styles.container} ${selected && styles.selected}`}
-    onClick={() => onClick(element.title.toLowerCase())}
-  >
-    <img src={element.icon} alt="" aria-hidden="true" className={styles.icon} />
-    <strong className={styles.title}>{element.title}</strong>
-    <p className={styles.price}>
-      {yearly ? element.price.yearly : element.price.monthly}
-    </p>
-    {yearly && <small className={styles.price__yearly}>2 months free</small>}
-  </li>
-);
+const PlanCard = ({ element }: PlanCardProps) => {
+  const { priceModel, plan } = useAppSelector((state) => state.plans);
+  const dispatch = useAppDispatch();
+
+  const isYearlyPlanSelected = priceModel === PriceModel.YEARLY;
+  const isPlanSelected = plan === element.title;
+
+  const handleCardClick = () => {
+    dispatch(selectPlan(element.title));
+  };
+
+  return (
+    <li
+      className={`${styles.container} ${isPlanSelected && styles.selected}`}
+      onClick={handleCardClick}
+    >
+      <img
+        src={element.icon}
+        alt=""
+        aria-hidden="true"
+        className={styles.icon}
+      />
+      <strong className={styles.title}>{element.title}</strong>
+      <p className={styles.price}>
+        {isYearlyPlanSelected ? element.price.yearly : element.price.monthly}
+      </p>
+      {isYearlyPlanSelected && (
+        <small className={styles.price__yearly}>2 months free</small>
+      )}
+    </li>
+  );
+};
 
 export default PlanCard;
--- src/components/Tabs/PlanTab/index.tsx
+++ src/components/Tabs/PlanTab/index.tsx
@@ -1,19 +1,14 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import Button from '~/components/Button';
 import PlanCard from '~/components/PlanCard';
 import TogglePill from '~/components/TogglePill';
 import PLAN_CARDS from '~/constants/planCards';
-import PriceModel from '~/constants/priceModel';
 import { StepsContext } from '~/context/StepsContext';
-import { useAppSelector } from '~/hooks';
 import Container from '../Container';
 import styles from './styles.module.scss';
 
 const PlanTab = () => {
-  const [selectedCard, setSelectedCard] = useState<string | null>(null);
   const { activeStep, setActiveStep } = useContext(StepsContext);
-
-  const { priceModel } = useAppSelector((state) => state.plans);
 
   return (
     <Container>
@@ -23,13 +18,7 @@ const PlanTab = () => {
       </p>
       <ul className={styles.plan__list}>
         {PLAN_CARDS.map((item) => (
-          <PlanCard
-            element={item}
-            key={item.title}
-            onClick={setSelectedCard}
-            selected={selectedCard === item.title.toLowerCase()}
-            yearly={priceModel === PriceModel.YEARLY}
-          />
+          <PlanCard element={item} key={item.title} />
         ))}
       </ul>
       <div className={styles.price__container}>
